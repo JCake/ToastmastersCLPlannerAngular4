e829939f48e104bb474da1db7b1a28071b4aef06
Jessica C <jcake2@gmail.com>
2017-06-11 17:23:42 -0500

    Redo of project using angular cli quickstart instead of local-only quickstart.

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,15 +1,14 @@
-import { browser, element, by } from 'protractor';
+import { ClPlannerPage } from './app.po';
 
-describe('QuickStart E2E Tests', function () {
+describe('cl-planner App', () => {
+  let page: ClPlannerPage;
 
-  let expectedMsg = 'Toastmaster CL Planner';
-
-  beforeEach(function () {
-    browser.get('');
+  beforeEach(() => {
+    page = new ClPlannerPage();
   });
 
-  it('should display: ' + expectedMsg, function () {
-    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
   });
-
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  templateUrl: 'app/index.html'
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent  { 
   name = 'Jessica';
