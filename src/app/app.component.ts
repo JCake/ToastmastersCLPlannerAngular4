fade9f95fb17fc53784bba65fd3b6b38616f00b0
Jessica C <jcake2@gmail.com>
2017-06-11 17:33:46 -0500

    Corrections to deployment items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent  { 
-  name = 'Jessica';
+  showingInstructions = false;
   plannedRoles: DefinedRole[] = [];
   roles: String[];
   selectedRole: String;
